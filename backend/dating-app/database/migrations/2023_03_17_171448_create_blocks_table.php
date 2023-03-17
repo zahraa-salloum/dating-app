78e4fa03de7fe6d30cf5b8a4ceb86d5ccbd8b03a
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-03-17 20:05:59 +0200

    Created database tables

--- backend/dating-app/database/migrations/2023_03_17_171448_create_blocks_table.php
+++ backend/dating-app/database/migrations/2023_03_17_171448_create_blocks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateBlocksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->integer('user_id_blocked')->unsigned();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('blocks');
     }
 }
---
+++ backend/dating-app/database/migrations/2023_03_17_171619_create_users_info_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_info', function (Blueprint $table) {
+            $table->id();
+            $table->text("bio");
+            $table->string("status");
+            $table->string("education");
+            $table->string("job");
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_info');
+    }
+}
